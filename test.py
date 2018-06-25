a6cbfe715bfba0e5174e62779872bf7bc5912335
Unknown <tekikralj011@gmail.com>
2018-06-25 17:56:25 +0200

    histogram equalisation done
    histogram equalisation done, added test images, still a lot of hardkoded stuff corrected

--- test.py
+++ test.py
@@ -2,12 +2,22 @@ import cv2 as cv
 import numpy as np 
 from matplotlib import pyplot as plt 
 
-img = cv.imread("test.jpg")
-
+img = cv.imread("test_images/majin_kanon/IMG_5156.JPG", 0)
+cv.imwrite('greyscaled-pics/original_greyscale.jpg', img)
 hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 
+####odredjivanje kumulativne verovatnoce piksela
 cdf_original = hist.cumsum()
 cdf_modified = cdf_original * hist.max() / cdf_original.max()
+###
+
+
+######
+cdf_modified2 = np.ma.masked_equal(cdf_original, 0)
+cdf_modified2 = (cdf_modified2 - cdf_modified2.min())*255/(cdf_modified2.max()-cdf_modified2.min())
+cdf_original = np.ma.filled(cdf_modified2, 0).astype('uint8')
+######
+
 
 #first picture graph
 plt.plot(cdf_modified, color = 'b')
@@ -16,14 +26,6 @@ plt.xlim([0, 256])
 plt.legend(('cdf', 'histogram'))
 plt.show()
 
-
-cdf_modified2 = np.ma.masked_equal(cdf_original, 0)
-cdf_modified2 = (cdf_modified2 - cdf_modified2.min())*255/(cdf_modified2.max()-cdf_modified2.min())
-cdf_original = np.ma.filled(cdf_modified2, 0).astype('uint8')
-
-img2 = cdf_original[img]
-cv.imwrite("modified.jpg", img2)
-
 #modified picture graph
 plt.plot(cdf_modified2, color = 'b')
 plt.hist(img.flatten(), 256, [0, 256], color = 'r')
@@ -31,9 +33,11 @@ plt.xlim([0, 256])
 plt.legend(('cdf2', 'histogram'))
 plt.show()
 
+img2 = cdf_original[img]
 cv.imshow("modified", img2)
 cv.imshow("original", img)
+cv.namedWindow('original', cv.WINDOW_NORMAL)
+cv.namedWindow('modified', cv.WINDOW_NORMAL)
 
-key = cv.waitKey()
-if key == 27:
-    cv.destroyAllWindows()
+cv.imwrite("greyscaled-pics/modified.jpg", img2)
+cv.waitKey()
