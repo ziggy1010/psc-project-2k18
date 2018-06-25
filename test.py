bd71c9e7ff0a2ae80677c161b4021909b8bdc8b2
Unknown <tekikralj011@gmail.com>
2018-06-25 11:11:52 +0200

    plot of the modified picture, and save

--- test.py
+++ test.py
@@ -4,14 +4,15 @@ from matplotlib import pyplot as plt
 
 img = cv.imread("test.jpg")
 
-hist, bins = np.histogram(img.flatten(), 512, [0, 512])
+hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 
 cdf_original = hist.cumsum()
 cdf_modified = cdf_original * hist.max() / cdf_original.max()
 
+#first picture graph
 plt.plot(cdf_modified, color = 'b')
-plt.hist(img.flatten(), 512, [0, 512], color = 'r')
-plt.xlim([0, 512])
+plt.hist(img.flatten(), 256, [0, 256], color = 'r')
+plt.xlim([0, 256])
 plt.legend(('cdf', 'histogram'))
 plt.show()
 
@@ -21,7 +22,18 @@ cdf_modifed2 = (cdf_modified2 - cdf_modified2.min())*255/(cdf_modified2.max()-cd
 cdf_original = np.ma.filled(cdf_modified2, 0).astype('uint8')
 
 img2 = cdf_original[img]
+cv.imwrite("modified.jpg", img2)
+
+#modified picture graph
+plt.plot(cdf_modified2, color = 'b')
+plt.hist(img.flatten(), 256, [0, 256], color = 'r')
+plt.xlim([0, 256])
+plt.legend(('cdf2', 'histogram'))
+plt.show()
 
 cv.imshow("modified", img2)
 cv.imshow("original", img)
-cv.waitKey()
+
+key = cv.waitKey()
+if key == 27:
+    cv.destroyAllWindows()
