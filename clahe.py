d6f9e0a54b9ac24df826263fc96ca3f0864dd1d3
Unknown <tekikralj011@gmail.com>
2018-06-29 17:35:58 +0200

    adaBoost ain't workin'
    not enough values to unpack (expected 2, got 1)
    model = learner.train(features, labels)

--- clahe.py
+++ clahe.py
@@ -1,8 +1,15 @@
-import cv2 as cv 
+import cv2 as cv; from glob import glob
 import numpy as np
 
-img = cv.imread('test_images/majin_kanon/IMG_5141.JPG', 0)
+#img = cv.imread('test_images/majin_kanon/IMG_5141.JPG', 0)
 
-clahe = cv.createCLAHE(clipLimit = 2.0, tileGridSize = (8, 8))
-cli = clahe.apply(img)
-cv.imwrite('kurac2.jpg', cli)
+def clahe(img, name):
+    clahe = cv.createCLAHE(clipLimit = 2.0, tileGridSize = (8, 8))
+    cli = clahe.apply(img)
+    cv.imwrite("greyscaled-pics/second-method/" + name, cli)
+
+for fn in glob("test_images/majin_kanon/*"):
+    print(fn)
+    img = cv.imread(fn, 0)
+    clahe(img, fn.split("/")[-1])
+#cv.imwrite('kurac2.jpg', cli)
