69e2233db7b37266fa0e0ac258a66854a26c5884
Stefan Nožinić <stefan@lugons.org>
2018-07-03 16:37:44 +0200

    fixed recursion depth bug

--- adaBoost.py
+++ adaBoost.py
@@ -1,4 +1,11 @@
 import cv2 as cv; import numpy as np; from glob import glob
+from clahe import * 
+from histEqualisation import * 
+from linNormalization import * 
+
+clahe_adaboost = lambda x: clahe(x, "kurcic")
+he_adaboost = lambda x: histogram_equalisation(x, "kurcic2")
+ln_adaboost = lambda x: linNormalization(x, "kurcic3")
 
 wanted_output1 = np.array([1, 0, 0, 0, 0]) #majin kanon
 wanted_output2 = np.array([0, 1, 0, 0, 0]) #nikolin kanon
@@ -13,22 +20,28 @@ def frange(start, stop, step): #float in for loop
         yield x
         x += step
 
-min_error = 99999
-def calcError(weight1, weight2, weight3): #error function
-    for i in range(img_num):
-        epsilon = ((linCombination(img) - wanted_output1)**2)/img_num
-        return epsilon
+def f(x, w1, w2, w3, f1, f2, f3):
+    return w1*f1(x) + w2*f2(x) + w3*f3(x)
 
-def linCombination(img): #linear combination of methods
+def calcError(weight1, weight2, weight3, f1, f2, f3): #error function
+    epsilon = 0
+    for i in range(img_num):
+        epsilon += ((mama(img.flatten(), weight1, weight2, weight3, f1, f2, f3) - wanted_output1)**2)
+    return epsilon / img_num
+
+def linCombination(img, f1, f2, f3): #linear combination of methods
+    min_error = 10**9
     for weight1 in frange(0, 10, 0.1):
         for weight2 in frange(0, 10, 0.1):
             for weight3 in frange(0, 10, 0.1):
                 #lin_combination = weight1*img.flatten()+weight2*img.flatten()+weight3*img.flatten()
-                error = calcError(weight1, weight2, weight3) 
-                if error < min_error: min_error = error; print(min_error)
+                error = calcError(weight1, weight2, weight3, f1, f2, f3) 
+                if error < min_error: 
+                    min_error = error
+                    print(min_error)
 
 
 for fn in glob("greyscaled-pics/first_method/all_pics/*"):
     img = cv.imread(fn, 0)
     img_arr = img.flatten()
-    linCombination(img_arr)
+    linCombination(img_arr, clahe_adaboost, he_adaboost, ln_adaboost)
